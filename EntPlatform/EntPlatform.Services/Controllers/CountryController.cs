fe51ab39c3a81e14577961ffaa2f42c449748310
mfadzil <mfadzil@HTP7571-01>
2018-07-26 11:22:26 +0800

    new controller:Common Controller

---
+++ EntPlatform/EntPlatform.Services/Controllers/CountryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EntPlatform.Services.Models;
+using System.Linq;
+
+namespace EntPlatform.Services.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly EntPlaformContext _context;
+        public CountryController(EntPlaformContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var listofcountry = from x in _context.Country
+                                orderby x.Name
+                                select x;
+
+
+            return View(listofcountry.ToList());
+        }
+    }
+}
